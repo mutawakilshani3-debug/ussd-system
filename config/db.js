2e5cafc3da0d717ccf9e0c8b3362b5261c6cfad0
Shani Abdul Mutawakil <mutawakilshani3@gmail.com>
2026-07-01 18:44:14 -0700

    fix db connection and login input

--- config/db.js
+++ config/db.js
@@ -2,11 +2,12 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  host: process.env.DB_HOST || '127.0.0.1',
+  host: process.env.DB_HOST,
   port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
-  database: process.env.DB_NAME || 'ussd_result_attendance',
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  ssl: { rejectUnauthorized: false },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
